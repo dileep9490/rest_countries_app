8a2c2d362c7366ecf35dacabe6d7784177c5af65
dileep kumar <63330310+dileep9490@users.noreply.github.com>
2022-02-18 12:05:48 +0530

    colors added

---
+++ lib/const.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+///Dark Mode Elements
+const Color darkBlue = Color(0xff2b3945);
+
+///Dark Mode Background
+const Color veryDarkBlue = Color(0xff202c37);
+
+///Light Mode Text
+const Color veryDarkBlue2 = Color(0xff111517);
+
+///Light Mode Input
+const Color darkGray = Color(0xff858585);
+
+///Light Mode Background
+const Color veryLightGray = Color(0xfffafafa);
+
+///Dark Mode Text & Light Mode Elements
+const Color white = Color(0xffffffff);
