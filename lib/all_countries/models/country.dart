5ecd45a948afc88b6e2e5ea1fa7e007e1d0fe7d2
dileep kumar <63330310+dileep9490@users.noreply.github.com>
2022-03-02 23:55:01 +0530

    homepage country model

---
+++ lib/all_countries/models/country.dart
@@ -0,0 +1,31 @@
+
+
+class Country {
+  final String name;
+  final int population;
+  final List<String> capital;
+  final String region;
+  final String flagUrl;
+  final String id;
+  Country({
+    required this.name,
+    required this.population,
+    required this.capital,
+    required this.region,
+    required this.id,
+    required this.flagUrl,
+  });
+
+  factory Country.fromMap(Map<String, dynamic> map) {
+    return Country(
+      name: map['name']['common'] ?? '',
+      population: map['population']?.toInt() ?? 0,
+      capital: map['capital'] ?? '',
+      region: map['region'] ?? '',
+      id: map['cca2'] ?? '',
+      flagUrl: map['flags']['png']
+    );
+  }
+
+
+}
