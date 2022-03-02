28eb3eb50609376083c6cd318c620c614c9d5765
dileep kumar <63330310+dileep9490@users.noreply.github.com>
2022-03-02 19:38:04 +0530

    completed app bar

--- lib/all_countries/views/homepage.dart
+++ lib/all_countries/views/homepage.dart
@@ -1,12 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:rest_countries_app/const.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: HomePageView(),
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(74),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 10),
+          child: AppBar(
+            title: Text('Where in the world ?',
+                style: Theme.of(context).textTheme.bodyLarge),
+            actions: [
+              Padding(
+                padding: const EdgeInsets.only(right: 16),
+                child: GestureDetector(
+                  onTap: () {
+                    //TODO:implement dark mode
+                  },
+                  child: Row(children: [
+                    const Icon(
+                      Icons.dark_mode_outlined,
+                      color: veryDarkBlue2,
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Text(
+                      'Dark Mode',
+                      style: Theme.of(context).textTheme.bodyMedium,
+                    )
+                  ]),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: const HomePageView(),
     );
   }
 }
@@ -16,8 +50,11 @@ class HomePageView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("hello there"),
+    return Center(
+      child: Text(
+        "hello there",
+        style: Theme.of(context).textTheme.bodyMedium!.copyWith(fontSize: 20),
+      ),
     );
   }
 }
--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:rest_countries_app/all_countries/views/homepage.dart';
+import 'package:rest_countries_app/const.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -9,7 +10,20 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "Countries App",
       home: const HomePage(),
-      theme: ThemeData(fontFamily: 'Nunito_Sans'),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'Nunito_Sans',
+        backgroundColor: veryLightGray,
+        appBarTheme: const AppBarTheme(backgroundColor: veryLightGray),
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(
+              color: veryDarkBlue2, fontWeight: FontWeight.w800, fontSize: 18),
+          bodyMedium:
+              TextStyle(color: veryDarkBlue2, fontWeight: FontWeight.w600),
+          bodySmall:
+              TextStyle(color: veryDarkBlue2, fontWeight: FontWeight.w300),
+        ),
+      ),
     );
   }
 }
