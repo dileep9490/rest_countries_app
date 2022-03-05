2c37339a0afe35398a4350f4dd12cfc4917815aa
dileep kumar <63330310+dileep9490@users.noreply.github.com>
2022-03-05 18:41:51 +0530

    drop down menu edited

--- lib/all_countries/widgets/drop_down_menu.dart
+++ lib/all_countries/widgets/drop_down_menu.dart
@@ -1,32 +1,53 @@
 import 'package:flutter/material.dart';
 
-class DropDownMenu extends StatelessWidget {
+class DropDownMenu extends StatefulWidget {
   const DropDownMenu({
     Key? key,
   }) : super(key: key);
 
-  List<DropdownMenuItem<String>> get dropdownItems {
-    List<DropdownMenuItem<String>> menuItems = const [
-      DropdownMenuItem(child: Text("USA"), value: "1"),
-      DropdownMenuItem(child: Text("Canada"), value: "2"),
-      DropdownMenuItem(child: Text("Brazil"), value: "3"),
-      DropdownMenuItem(child: Text("England"), value: "4"),
-    ];
-    return menuItems;
-  }
+  @override
+  State<DropDownMenu> createState() => _DropDownMenuState();
+}
+
+class _DropDownMenuState extends State<DropDownMenu> {
+  String? value;
 
   @override
   Widget build(BuildContext context) {
-    String selectedvalue = '1';
+    TextStyle menuItemTextStyle =
+        Theme.of(context).textTheme.bodyMedium!.copyWith(fontSize: 16);
+    List<DropdownMenuItem<String>> menuItems = const [
+      DropdownMenuItem(child: Text("All"), value: "all"),
+      DropdownMenuItem(child: Text("America"), value: "region/america"),
+      DropdownMenuItem(child: Text("Africa"), value: "region/africa"),
+      DropdownMenuItem(child: Text("Asia"), value: "region/asia"),
+      DropdownMenuItem(child: Text("Europe"), value: "region/eruope"),
+      DropdownMenuItem(child: Text("Oceania"), value: "region/oceania"),
+    ];
+
     return Container(
       alignment: Alignment.centerLeft,
-      child: DropdownButton(
-        items: dropdownItems,
-        onChanged: (String? value) {
-          //TODO: work with this later
-          selectedvalue = value as String;
-        },
-        value: selectedvalue,
+      padding: EdgeInsets.only(top: 20),
+      child: Card(
+        child: DropdownButtonHideUnderline(
+          child: Container(
+            margin: const EdgeInsets.all(10),
+            height: 30,
+            child: DropdownButton(
+              style: menuItemTextStyle,
+              hint: Text(
+                'Filter By Region',
+                style: menuItemTextStyle,
+              ),
+              items: menuItems,
+              onChanged: (value) {
+                setState(() => this.value = value as String);
+              },
+              value: value,
+              icon: const Icon(Icons.keyboard_arrow_down_rounded),
+            ),
+          ),
+        ),
       ),
     );
   }
