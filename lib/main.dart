bde4aa783691ee44c431f2da61d8522349b40352
dileep <dileepgudimetla9490@gmail.com>
2022-09-26 17:28:54 +0530

    added comment

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'theme/bloc/app_theme_bloc.dart';
 void main(List<String> args) {
   runApp(BlocProvider(
     create: (context) => AppThemeBloc(),
+    //using bloc
     child: const MyApp(),
   ));
 }
